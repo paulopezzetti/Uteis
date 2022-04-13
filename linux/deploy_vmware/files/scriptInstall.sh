364f21b148f43ee0231c3b1f01733789ffb61e8f
paulo.pezzetti <paulo.pezzetti@gazin.com.br>
2022-04-13 11:43:18 -0300

    removndo arquivos

--- linux/deploy_vmware/files/scriptInstall.sh
+++
@@ -1,9 +0,0 @@
-mkdir /root/agent
-cd /root/agent
-wget http://git.unicesumar.edu.br/Scripts/Linux/raw/master/iDRAgent/agent_installer.sh
-wget http://git.unicesumar.edu.br/Scripts/Linux/raw/master/iDRAgent/cafile.pem
-wget http://git.unicesumar.edu.br/Scripts/Linux/raw/master/iDRAgent/client.crt
-wget http://git.unicesumar.edu.br/Scripts/Linux/raw/master/iDRAgent/client.key
-wget http://git.unicesumar.edu.br/Scripts/Linux/raw/master/iDRAgent/config.json
-chmod u+x /root/agent/agent_installer.sh && /root/agent/agent_installer.sh install_start
-rm -rf /root/agent
